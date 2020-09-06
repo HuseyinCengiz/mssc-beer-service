e673c3708d3ab82b1481af2ea997e87eba7c77d8
Huseyin Cengiz <hseyin.cengiz@hotmail.com>
2020-09-06 17:52:06 +0300

    Added Initial Project. Closes #1.

---
+++ src/test/java/hc/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hc.springframework.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
